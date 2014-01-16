1c2a290741776e3f35c04cfdbcb643d91609b34d
bilalliberty <dominosliberty@gmail.com>
2014-01-16 22:40:08 +0300

    Rename camera hal to us camera wrapper

--- pyramid-vendor-blobs.mk
+++ pyramid-vendor-blobs.mk
@@ -81,7 +81,7 @@ PRODUCT_COPY_FILES += \
   vendor/htc-extra/pyramid/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
   vendor/htc-extra/pyramid/proprietary/lib/libqdp.so:system/lib/libqdp.so \
   vendor/htc-extra/pyramid/proprietary/lib/libqmi.so:system/lib/libqmi.so \
-  vendor/htc-extra/pyramid/proprietary/lib/hw/camera.msm8660.so:system/lib/hw/camera.msm8660.so \
+  vendor/htc-extra/pyramid/proprietary/lib/hw/camera.msm8660.so:system/lib/hw/vendor-camera.msm8660.so \
   vendor/htc-extra/pyramid/proprietary/lib/hw/sensors.pyramid.so:system/lib/hw/sensors.pyramid.so \
   vendor/htc-extra/pyramid/proprietary/lib/libmllite.so:system/lib/libmllite.so \
   vendor/htc-extra/pyramid/proprietary/lib/libmlplatform.so:system/lib/libmlplatform.so \
